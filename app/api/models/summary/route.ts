ec359e2949be6bcd71ee30b3def5490ea8f710a8
Filip Cieślik <fc@Filips-MacBook-Air.local>
2024-01-17 15:29:22 +0100

    dadasdsadsa

--- app/api/models/summary/route.ts
+++ app/api/models/summary/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
+  const dupa = process.env.DUPA_API_KEY;
   return NextResponse.json({ message: "Hello world!!!" });
 }
